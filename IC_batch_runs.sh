d956fb13a5814ee2ea976e46efbef5480941b08b
Nathan Willis <nwillis@ucmerced.edu>
2026-02-11 13:00:27 -0700

    Added a shell script (IC_batch_runs.sh) to run over a single batch. Added a python script (create_batches.py) to generate branches

---
+++ IC_batch_runs.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+gcc -O3 -march=native -o IC_Sedimentation2026Feb11.out TwoCurrSWEsolver.c # Compile main
+
+# Get batch index from first argument and set up batch_file
+batch_index=$1
+batch_file="batch_${batch_index}.txt"
+
+# Find all PIDS to kill 
+#   ps aux | grep IC_Sedim | grep -v grep
+
+# look for number of processes the computer can run
+#   sysctl -n hw.ncpu
+
+# do
+# the ampersand is the magic that runs it in parallel
+# inputs go in this order N Reynolds CFL h_min sharp U_s c2init h2init
+
+max_jobs=256 # max parallel jobs allowed
+while read param1 param2; do
+    ./IC_Sedimentation2026Feb11.out 7000 1000 0.1 0.0001 50. 0.01 $param1 $param2 &
+    echo "$param1 $param2" >> SedimentationInitialConditionTest_2026Feb11/progress.log
+    while (( $(jobs -r | wc -l) >= max_jobs )); do
+      sleep 5
+    done
+done < "$batch_file"
+
+wait
+echo "Batch $batch_index completed."
---
+++ create_batches.py
@@ -0,0 +1,17 @@
+def create_batches(minx,dx,maxx,miny,dy,maxy):
+    x = minx
+    L = []
+    while x<=maxx+1e-10:
+        y = miny
+        while y<=maxy+1e-10:
+            if x*y<=1+1e-8: L.append([x,y])
+            y+=dy
+        x+=dx
+
+    R = list(range(0,len(L),int(len(L)/(int(len(L)/256)+1))+1)) + [len(L)]
+    for i in range(len(R)-1):
+        print(i,R[i],R[i+1])
+        l = L[R[i]:R[i+1]]
+        with open('./batch_%i.txt'%i,'w') as f:
+            for params in l:
+                f.write('%0.2f %0.2f\n'%(params[0],params[1]))
