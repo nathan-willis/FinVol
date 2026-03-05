386458ab156c8c850f3873c83f27c171ab226134
Nathan Willis <nwillis@Francoiss-Mac-Pro.local>
2026-03-05 14:49:16 -0800

    Updated IC_batch_runs.sh and edited run details in TwoCurrSWEsolver.c

--- IC_batch_runs.sh
+++ IC_batch_runs.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-gcc -O3 -march=native -o IC_Sedimentation2026Feb11.out TwoCurrSWEsolver.c # Compile main
+gcc -O3 -march=native -o DepositionExamplePlots_Mar3.out TwoCurrSWEsolver.c # Compile main
 
 # Get batch index from first argument and set up batch_file
-batch_index=$1
-batch_file="batch_${batch_index}.txt"
+batch_file="$1"
 
 # Find all PIDS to kill 
-#   ps aux | grep IC_Sedim | grep -v grep
+#   ps aux | grep DepositionExamplePlots_Mar3| grep -v grep
 
 # look for number of processes the computer can run
 #   sysctl -n hw.ncpu
@@ -16,10 +15,12 @@ batch_file="batch_${batch_index}.txt"
 # the ampersand is the magic that runs it in parallel
 # inputs go in this order N Reynolds CFL h_min sharp U_s c2init h2init
 
-max_jobs=256 # max parallel jobs allowed
+# nohup ./IC_batch_runs.sh DepositExamplePlots_batch.txt &
+
+max_jobs=12 # max parallel jobs allowed
 while read param1 param2; do
-    ./IC_Sedimentation2026Feb11.out 28000 1000 0.1 0.0001 50. 0.01 $param1 $param2 &
-    echo "$param1 $param2" >> Feb11_2026_SedimentationInitialConditionTest/progress.log
+    ./DepositionExamplePlots_Mar3.out 28000 1000 0.1 0.0001 200. 0.01 $param1 $param2 &
+    echo "$param1 $param2" >> Mar3_DepositionExamplePlots/progress.log
     while (( $(jobs -r | wc -l) >= max_jobs )); do
       sleep 5
     done
